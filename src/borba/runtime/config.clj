325879e6acc80be1e4c78e4ab56301f5b6565880
Borba <dev_andre.filipe@outlook.com>
2026-03-13 01:25:32 -0300

    fix deep-merge aero

--- src/borba/runtime/config.clj
+++ src/borba/runtime/config.clj
@@ -3,6 +3,15 @@
    [aero.core :as aero]
    [clojure.java.io :as io]))
 
+(defmethod aero/reader 'deep-merge
+  [opts _ values]
+  (apply merge-with
+         (fn deep-merge-fn [v1 v2]
+           (if (and (map? v1) (map? v2))
+             (merge-with deep-merge-fn v1 v2)
+             v2))
+         values))
+
 (defn load-config
   [profile]
   (aero/read-config
