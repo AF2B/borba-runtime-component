a8a14f8da052c0eb9193ca7eedde795563c28311
Borba <dev_andre.filipe@outlook.com>
2026-03-12 21:19:38 -0300

    refactor to handle the -main in a unique component

--- src/borba/runtime/main.clj
+++ src/borba/runtime/main.clj
@@ -1,19 +1,24 @@
 (ns borba.runtime.main
-  (:require
-   [borba.runtime.config :as config]
-   [borba.runtime.system :as system]))
+  (:gen-class)
+  (:require [aero.core :as aero]
+            [borba.runtime.config :as config]
+            [borba.runtime.system :as system]
+            [clojure.java.io :as io]))
 
 (defn run
   [{:keys [profile]}]
-
   (let [cfg (config/load-config profile)]
-
     (system/start! cfg)
-
     (.addShutdownHook
      (Runtime/getRuntime)
      (Thread. system/stop!))
-
     (println "Service started with profile:" profile)
-
     @(promise)))
+
+(defn -main
+  [& [profile]]
+  (let [base            (aero/read-config (io/resource "config/base.edn") {})
+        core-ns         (symbol (:service/core-ns base))
+        default-profile (name (:service/default-profile base "stag"))]
+    (require core-ns)
+    (run {:profile (keyword (or profile default-profile))})))
