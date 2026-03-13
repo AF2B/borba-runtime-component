80ec57480ca9c2eb2c7f9edf8db237c98af1f166
Borba <dev_andre.filipe@outlook.com>
2026-03-13 01:17:00 -0300

    simplify configedn

--- src/borba/runtime/config.clj
+++ src/borba/runtime/config.clj
@@ -3,20 +3,8 @@
    [aero.core :as aero]
    [clojure.java.io :as io]))
 
-(defn read-config
-  [resource profile]
-
-  (aero/read-config
-   resource
-   {:profile profile}))
-
 (defn load-config
   [profile]
-
-  (let [base (io/resource "config/base.edn")
-        env  (io/resource (str "config/" (name profile) ".edn"))]
-
-    (merge
-     (read-config base profile)
-     (when env
-       (read-config env profile)))))
+  (aero/read-config
+   (io/resource "config.edn")
+   {:profile profile}))
